606240b59f82f294fac5c37ac3f87345da82d0f4
pubuduarosha <aroshapubudu@gmail.com>
2023-10-28 18:13:27 +0530

    Sample done

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+from flask import Flask, render_template, request
+server = Flask(__name__)
+
+
+@server.route('/mydetail', methods=['GET', 'POST'])
+def show_my_details():
+
+    if request.method == 'POST':
+        form_data = request.form
+        print("Form data: ", form_data)
+        name = form_data['name']
+        email = form_data['email']
+        print("Name: ", name)
+        print("Email: ", email)
+        return render_template("my/show.html", name=name, email=email)
+
+    return render_template("my/detail.html")
+
+
+server.run(debug=True)
